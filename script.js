537ce86582194cd66a4214fce57292a7de6e0ac3
Lucas Cuppi <lucascuppi@gmail.com>
2026-04-09 15:28:25 -0300

    SNKRS

---
+++ script.js
@@ -0,0 +1,58 @@
+const next = document.querySelector(".btn-nav.next");
+const back = document.querySelector(".btn-nav.back");
+const slides = document.querySelectorAll(".slide");
+const bolinhas = document.querySelectorAll(".bolinha");
+
+let contador = 0;
+
+function passarSlide() {
+  const slideAtivo = document.querySelector(".slide.ativo");
+  const bolinhasAtivas = document.querySelectorAll(".bolinha.active");
+  slideAtivo.classList.remove("ativo");
+  bolinhasAtivas.forEach((bolinha) => {
+    bolinha.classList.remove("active");
+  });
+  if (contador == 2) {
+    contador = 0;
+  } else {
+    contador = contador + 1;
+  }
+  slides[contador].classList.add("ativo");
+  bolinhas[contador].classList.add("active");
+}
+
+function voltarSlide() {
+  const slideAtivo = document.querySelector(".slide.ativo");
+  const bolinhasAtivas = document.querySelectorAll(".bolinha.active");
+  slideAtivo.classList.remove("ativo");
+  bolinhasAtivas.forEach((bolinha) => {
+    bolinha.classList.remove("active");
+  });
+  if (contador == 0) {
+    contador = 2;
+  } else {
+    contador = contador - 1;
+  }
+  slides[contador].classList.add("ativo");
+  bolinhas[contador].classList.add("active");
+}
+
+back.addEventListener("click", voltarSlide);
+next.addEventListener("click", passarSlide);
+
+const vitrine = document.querySelector(".rolagem-cards");
+const btnDir = document.querySelector("#right");
+const btnEsq = document.querySelector("#left");
+
+const passo = 480;
+
+function rolarDireita() {
+  vitrine.scrollLeft += passo;
+}
+
+function rolarEsquerda() {
+  vitrine.scrollLeft -= passo;
+}
+
+btnDir.addEventListener("click", rolarDireita);
+btnEsq.addEventListener("click", rolarEsquerda);
